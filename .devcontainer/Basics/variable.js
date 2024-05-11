b5cbb06021d0e43eaac18e280cf8467287af1d77
Sakshi Sinha <154058910+Sakuu311@users.noreply.github.com>
2024-05-11 20:51:01 +0000

    c

---
+++ .devcontainer/Basics/variable.js
@@ -0,0 +1,12 @@
+const accountId=13312
+let accountEmail="Sakshi@google.com"
+var accountPassword="1234"
+accountCity="Jaipur"
+
+//accountId=2
+ accountEmail="S@google.com"
+ accountPassword="6543"
+accountCity="Delhi"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
