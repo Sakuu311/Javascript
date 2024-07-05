afc1751c6073246ab69d28b776420e9cf9e5aa88
Sakshi Sinha <154058910+Sakuu311@users.noreply.github.com>
2024-07-05 11:32:49 +0000

    data

---
+++ New/basics/datatypes.js
@@ -0,0 +1,8 @@
+"Use Strict"; //new version of js
+//alert(3+3) //we r using nodejs not browser
+let age=5;
+let big=6523327687253;
+let name="Sakshi"
+console.log(typeof "Sakshi")
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
