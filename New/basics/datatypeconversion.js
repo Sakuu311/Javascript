d79f33ce84af1ea4c7136b12f7c7a5578dfc1798
Sakshi Sinha <154058910+Sakuu311@users.noreply.github.com>
2024-07-06 09:05:09 +0000

    done

---
+++ New/basics/datatypeconversion.js
@@ -0,0 +1,25 @@
+//null
+let score = null 
+let value = Number(score)
+console.log(typeof "value")
+console.log(value) //sometimes this will happened that Nan(not a number) gives output.
+//numberandstring
+let age = "28abc"
+let valuenew = Number(age)
+console.log(typeof "valuenew")
+console.log(valuenew)
+//undefined
+let un = undefined //undefined
+let valueinn = Number(un)
+console.log(typeof "valueinn")
+console.log(valueinn)
+//boolean
+let tf = true //true
+let valu = Number(tf)
+console.log(typeof "valu")
+console.log(valu)
+//name can not convert into number
+let name = "Sakshi" //true
+let valuee = Number(name)
+console.log(typeof "valuee")
+console.log(valuee)
